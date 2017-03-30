33ae4284fb8cb3a540ace612c1ea2fbe310e1e47
Unknown <matl1995@correo.ugr.es>
2017-03-30 11:17:42 +0200

    Clase Listado comentada(P1)

--- P1/src/ListadoTest.java
+++ P1/src/ListadoTest.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.Map;
 
 /**
+ * Clase que se encarga de testear el código creado
  * @author Miguel Ángel Torres López
  */
 public class ListadoTest {
--- P1/src/listado/Listado.java
+++ P1/src/listado/Listado.java
@@ -12,12 +12,17 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
+ * Clase que crea la figura de Listado
  * @author Miguel Ángel Torres López
  */
 public class Listado {
     private Map<String, Empleado> lista=new HashMap<>();
 
-    //Metodo constructor de la clase
+    /**
+     * Método constructor de la clase Listado
+     * @param ruta Contiene un string con la ruta al archivo con el listado de los empleados
+     * @throws IOException
+     */
     public Listado(String ruta) throws IOException {
         Stream<String> lineas = Files.lines(Paths.get(ruta));
         lineas.forEach(linea -> {
@@ -25,7 +30,10 @@ public class Listado {
         });
     }
 
-    //Metodo auxiliar para el constructor de la clase
+    /**
+     * Método auxiliar para el constructor de la clase
+     * @param linea Contiene un string con una linea con los datos de un empleado
+     */
     public void crearEmpleado(String linea){
         Pattern pattern = Pattern.compile("(,)");
         List<String> infos = pattern.splitAsStream(linea).collect(Collectors.toList());
@@ -33,7 +41,11 @@ public class Listado {
         lista.put(infos.get(0),new Empleado(infos.get(0),infos.get(1),infos.get(2),infos.get(3)));
     }
 
-    //Metodo para cargar los archivos con las divisiones
+    /**
+     * Método para cargar los archivos con las divisiones
+     * @param ruta Contiene un string con la ruta al archivo que contiene la division y los dni que pertenecen a esta
+     * @throws IOException
+     */
     public void cargarArchivoAsignacionDivision(String ruta)  throws IOException {
         Stream<String> lineasDivision = Files.lines(Paths.get(ruta));
         String division = lineasDivision.findFirst().orElseGet(null);
@@ -43,7 +55,11 @@ public class Listado {
         });
     }
 
-    //Metodo auxiliar para el que carga los archivos con las divisiones
+    /**
+     * Método auxiliar para la carga de los archivos con las divisiones
+     * @param linea Contiene un string con la linea con un dni
+     * @param div Contiene un objeto de tipo Division con la division que se asignará los dnis
+     */
     public void asignarDivision(String linea, Division div)
     {
         Pattern pattern = Pattern.compile("\\s");
@@ -51,7 +67,11 @@ public class Listado {
         lista.get(infos.get(0)).setDivision(div);
     }
 
-    //Metodo para cargar los archivos con los departamentos
+    /**
+     * Método para cargar los archivos con los departamentos
+     * @param ruta Contiene un string con la ruta al archivo que contiene el departamento y los dni que pertenecen a este
+     * @throws IOException
+     */
     public void cargarArchivoAsignacionDepartamento(String ruta) throws IOException {
         Stream<String> lineasDepartamento = Files.lines(Paths.get(ruta));
         String departamento = lineasDepartamento.findFirst().orElseGet(null);
@@ -61,7 +81,11 @@ public class Listado {
         });
     }
 
-    //Metodo auxiliar para el que carga los archivos con los departamentos
+    /**
+     * Método auxiliar para la carga de los archivos con los departementos
+     * @param linea Contiene un string con la linea con un dni
+     * @param dep Contiene un objeto de tipo Departamento con el departamento que se asignará los dnis
+     */
     public void asignarDepartamento(String linea, Departamento dep)
     {
         Pattern pattern = Pattern.compile("\\s");
@@ -69,14 +93,21 @@ public class Listado {
         lista.get(infos.get(0)).setDepartamento(dep);
     }
 
-    //Metodo que devuelve el numero de empleados existentes
+    /**
+     * Método que devuelve el número de empleados existentes
+     * @return Devuelve un entero con el numero de empleados que hay en la lista
+     */
     public int obtenerNumeroEmpleados()
     {
         return lista.size();
     }
 
-    //Metodo que obtiene los contadores de empleados para cada departamento en funcion de
-    //la division que se le pasa como parametro
+    /**
+     * Método que obtiene los contadores de empleados para cada departamento en funcion de
+     * la division que se le pasa como parametro
+     * @param div Contiene un objeto de tipo Division con la division para la que calcularemos los contadores
+     * @return Devuelve un mapa con el contador asociado a cada departamento perteneciente a la division recibida como parametro
+     */
     public Map<Departamento, Long> obtenerContadoresDepartamento(Division div){
         Map<Departamento, Long> contedoresDepartamento=new HashMap<>();
         Stream<Departamento> departamentoStream=Stream.of(Departamento.values());
@@ -89,8 +120,11 @@ public class Listado {
         return contedoresDepartamento;
     }
 
-    //Metodo que obtiene los contadores de empleados para cada division, y cada departamento,
-    //es decir, para cada departamento de cada division devuelve un contador de empleados que hay en este
+    /**
+     * Método que obtiene los contadores de empleados para cada division, y cada departamento, es decir,
+     * para cada departamento de cada division devuelve un contador de empleados que hay en este
+     * @return Devuelve un mapa con cada division asociada a otro mapa con los departamentos de dicha division asociados a un contador
+     */
     public Map<Division, Map<Departamento, Long>> obtenerContadoresDivisionDepartamento(){
         Map<Division, Map<Departamento, Long>> contadoresDivisionDepartamento = new HashMap<>();
         Stream<Division> divisionStream=Stream.of(Division.values());
@@ -98,8 +132,10 @@ public class Listado {
         return contadoresDivisionDepartamento;
     }
 
-    //Metodo que se encarga de buscar los empleados que tienen una division asignada pero no un departamento
-    //y que devuelve una lista con esos empleados
+    /**
+     * Método que se encarga de buscar los empleados que tienen una division asignada pero no un departamento
+     * @return Devuelve una lista con los empleados que tienen una division asignada pero no un departamento
+     */
     public List<Empleado> buscarEmpleadosConDivisionSinDepartamento(){
         List<Empleado> listaEmpleadosSD=new ArrayList<>();
         lista.entrySet().stream().filter(empleado -> (
@@ -108,8 +144,10 @@ public class Listado {
         return listaEmpleadosSD;
     }
 
-    //Metodo que se encarga de buscar los empleados que no tienen una division asignada
-    //y que devuelve una lista con esos empleados
+    /**
+     * Método que se encarga de buscar los empleados que no tienen una division asignada
+     * @return Devuelve una lista con los empleados que no tienen una division asignada
+     */
     public List<Empleado> buscarEmpleadosSinDivision(){
         List<Empleado> listaEmpleadosSD=new ArrayList<>();
         lista.entrySet().stream().filter(empleado -> (
@@ -118,8 +156,11 @@ public class Listado {
         return listaEmpleadosSD;
     }
 
-    //Metodo que se encarga de buscar los empleados con una division especifica asignada
-    //que no tienen un departamento asignado y que devuelve una lista con esos empleados
+    /**
+     * Método que se encarga de buscar los empleados con una division especifica asignada que no tienen un departamento asignado
+     * @param div Contiene un objeto de tipo Division con la division para la que hay que buscar los empleados sin departamento
+     * @return Devuelve una lista con los empleados que pertenecen a la division especificada y que no tienen un departamento asignado
+     */
     public List<Empleado> buscarEmpleadosSinDepartamento(Division div){
         List<Empleado> listaEmpleadosSD=new ArrayList<>();
         lista.entrySet().stream().filter(empleado -> (
@@ -128,14 +169,20 @@ public class Listado {
         return listaEmpleadosSD;
     }
 
-    //Metodo que devuelve el contenido de la lista en un string
+    /**
+     * Método toString de el Listado sobre escrito para que muestre los datos como deseamos
+     * @return Devuelve un string con la información del listado
+     */
     @Override
     public String toString()
     {
        return lista.toString();
     }
 
-    //Metodo que comprueba si hay dnis repetidos entre los empleados y devuelve un booleano
+    /**
+     * Método que comprueba si hay dnis repetdos entre los empleados
+     * @return Devuelve un booleano con true en caso de que haya dnis repetidos y false en caso contrario
+     */
     public boolean hayDnisRepetidos(){
         List<String> listaDNI = lista.entrySet().stream().map(empleado -> empleado.getValue().getDni()).distinct().collect(Collectors.toList());
         if(listaDNI.size()!=lista.size())
@@ -144,8 +191,10 @@ public class Listado {
             return false;
     }
 
-    //Metodo que busca los empleados con dnis repetidos y los devuelve en un map que asocia el dni repetido
-    //con una lista que contiene los empleados que compartian ese dni repetido
+    /**
+     * Método que busca los empleados con dnis repetidos
+     * @return Devuelve un map que asocia el dni repetido con una lista que contiene a los empleados que comparten ese dni repetido
+     */
     public Map<String,List<Empleado>> obtenerDnisRepetidos(){
         Map<String,List<Empleado>> dnis = new HashMap<>();
         Map<String,List<Empleado>> dnisRepetidos = new HashMap<>();
@@ -163,7 +212,10 @@ public class Listado {
         return dnisRepetidos;
     }
 
-    //Metodo que comprueba si hay emails repetidos entre los empleados y devuelve un booleano
+    /**
+     * Método que comprueba si hay emails repetdos entre los empleados
+     * @return Devuelve un booleano con true en caso de que haya emails repetidos y false en caso contrario
+     */
     public boolean hayCorreosRepetidos(){
         List<String> listaEmail = lista.entrySet().stream().map(empleado -> empleado.getValue().getEmail()).distinct().collect(Collectors.toList());
         if(listaEmail.size()!=lista.size())
@@ -172,8 +224,10 @@ public class Listado {
             return false;
     }
 
-    //Metodo que busca los empleados con emails repetidos y los devuelve en un map que asocia el email repetido
-    //con una lista que contiene los empleados que compartian ese email repetido
+    /**
+     * Método que busca los empleados con emails repetidos
+     * @return Devuelve un map que asocia el email repetido con una lista que contiene a los empleados que comparten ese email repetido
+     */
     public Map<String,List<Empleado>> obtenerCorreosRepetidos(){
         Map<String,List<Empleado>> correos = new HashMap<>();
         Map<String,List<Empleado>> correosRepetidos = new HashMap<>();
