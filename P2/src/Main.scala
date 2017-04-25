63e473e83175ebb4fd4972961b6a5a2ae96d7c0e
Miguel Angel <matl1995@correo.ugr.es>
2017-04-25 09:43:07 +0200

    Modificaciones en el main (P2)

--- P2/src/Main.scala
+++ P2/src/Main.scala
@@ -37,8 +37,14 @@ object Main {
     * @return Devuelve el entero que tendrá el triangulo de pascal en esa fila y columna
     */
   def calcularValorTrianguloPascal(columna: Int, fila: Int): Int = {
-    if(columna==0||columna==fila) 1
-    else calcularValorTrianguloPascal(columna-1,fila-1)+calcularValorTrianguloPascal(columna,fila-1)
+    if(columna==0 || columna==fila)
+    {
+      return 1;
+    }
+    else
+    {
+      return calcularValorTrianguloPascal(columna - 1, fila - 1) + calcularValorTrianguloPascal(columna, fila - 1);
+    }
   }
 
   /**
@@ -48,56 +54,68 @@ object Main {
     * @return Devuelve un booleano que indica true si la cadena esta bien balanceada y false en caso contrario
     */
   def chequearBalance(cadena: List[Char]): Boolean = {
-    val resultadoParentesis=comprobarParentesis(cadena)
 
-    if(cadena.isEmpty || resultadoParentesis == 0)
+    //La siguiente función es auxiliar para comprobar si la cadena esta balanceada, recibe la cadena y en el caso
+    //de que este vacia devuelve un 0 directamente, si no lo esta comprobamos la cadena, al encontrarnos un "(" sumamos
+    //1 mas una llamada a la funcion recursiva y si encontramos un ")" restamos a 1 una llamada a la función recursiva,
+    //así, si por cada parentesis que abro sumo 1 y por cada parentesis que cierro resto 1, se queda en 0, por lo que
+    //si la función devuelve un 0 es que está balanceada, y en otro caso no. En el caso de que el elemento de la cadena
+    //no sea un parentesis simplemente se vuelve a llamar a la funcion para que compruebe el siguiente elemento.
+    def comprobarParentesis(cadena: List[Char]): Int = {
+      if(!cadena.isEmpty) {
+        if (cadena.head=='(') //En el caso de que la cadena contenga '(' tenemos un parentesis que se tiene que cerrar por lo que sumo uno y sigo comprobando
+        {
+          return 1+comprobarParentesis(cadena.tail);
+        }
+        else if (cadena.head == ')') //En caso de que la cadena contenga ')' cierro un parentesis por lo que resto 1 y sigo comprobando
+        {
+          return 1-comprobarParentesis(cadena.tail);
+        }
+        else //En caso de que no se cumplan los otros casos, es que no afecta al balanceo por lo que llamo a la funcion para seguir comprobando
+        {
+          return comprobarParentesis(cadena.tail);
+        }
+      }
+      else //En el caso de que la cadena esté vacia esta bien balanceada asi que devolvemos un 0
+      {
+        return 0;
+      }
+    }
+
+    if(comprobarParentesis(cadena)==0) //Si al llamar a la funcion devuelve un 0 es que está balanceada por lo que devuelvo true
     {
       return true;
     }
-    else
+    else //Si no ha devuelto un cero es que no está balanceada por lo que devuelvo false
     {
       return false;
     }
   }
 
-  def comprobarParentesis(cadena: List[Char]): Int = {
-    if(!cadena.isEmpty) {
-      if (cadena.last=='(') //En caso de que la cadena acabe con '(', ese parentesis no se puede cerrar por lo que va a estar mal balanceada, devuelvo directamente menos 1 y no compruebo nada mas
-      {
-        return -1;
-      }
-      else if (cadena.head=='(') //En el caso de que la cadena contenga '(' tenemos un parentesis que se tiene que cerrar por lo que sumo uno y sigo comprobando
-      {
-        return 1+comprobarParentesis(cadena.tail);
-      }
-      else if (cadena.head != ')' && cadena.head != '(') //En el caso de que no haya ni '(' ni ')', el elemento no importa para el balance por lo que sigo comprobando
-      {
-        return comprobarParentesis(cadena.tail);
-      }
-      else if (cadena.head == ')') //En caso de que la cadena contenga ')' cierro un parentesis por lo que resto 1 y sigo comprobando
-      {
-        return comprobarParentesis(cadena.tail)-1;
-      }
-      else //En caso de que no se cumplan los otros casos, es que
-      {
-        return 0;
-      }
-    }
-    else //En el caso de que la cadena esté vacia esta bien balanceada asi que devolvemos un 0
-    {
-      return 0;
-    }
-  }
-/*
+
   /**
     * Ejercicio 3: funcion para determinar las posibles formas de devolver el
     * cambio de una determinada cantidad con un conjunto de monedas
     *
-    * @param cantidad
-    * @param monedas
-    * @return contador de numero de vueltas posibles
+    * @param cantidad Recibe un entero con la cantidad para la que se quiere calcular los conjuntos de monedas con los que se puede conseguir
+    * @param monedas Recibe una lista de enteros con el valor de las monedas con las que se tienen que buscar esos conjuntos que sumen la cantidad recibida
+    * @return devuelve un entero que contiene el número de formas en las que se puede pagar la cantidad dada con las monedas dadas
     */
   def contarCambiosPosibles(cantidad: Int, monedas: List[Int]): Int = {
-     // A rellenar
-  }*/
+    if (cantidad==0) //Cuando me dan que la cantidad es 0, es porque ya he llegado a obtener el conjunto de monedas por lo que
+    {                //devuelvo un 1, ya que es una forma de conseguir la cantidad con un conjunto de monedas dado
+      return 1;
+    }
+    else if (cantidad<0 || monedas.isEmpty) //Cuando me dan que la cantidad es menor que 0, devuelvo un 0 ya que no hemos conseguido obtener la cantidad con ese conjunto de monedas
+    {                                       //Cuando me dan que no quedan mas monedas, devuelvo un 0, ya que aun me queda cantidad que cubrir pero ya no me quedan ponedas para cubrirla
+      return 0;
+    }
+    else //En otro caso vuelvo a llamar a la función pasandole la cantidad menos la moneda usada, y todas las monedas que tenia
+    {    //y le sumo, una llamada a la función pasandole la misma cantidad, pero quitando la primera moneda de la lista,
+         //así consigo
+      return contarCambiosPosibles(cantidad-monedas.head,monedas)+contarCambiosPosibles(cantidad,monedas.tail);
+    }
+  }
+
+  def busquedaBinariaGenerica()
 }
